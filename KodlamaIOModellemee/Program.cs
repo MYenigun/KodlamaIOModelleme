cb81a4ff8ef16db0540522e83573fece174aae41
ZafranBlue <enriq@ZAFRANBLUE>
2024-02-14 08:18:07 +0300

    Add project files.

---
+++ KodlamaIOModellemee/Program.cs
@@ -0,0 +1,9 @@
+﻿using KodlamaIoModelleme.DataAccess.Concretes.InMemory;
+using KodlamaIoModelleme.Entities;
+
+ImInstructorDal imInstructorDal = new ImInstructorDal();
+
+foreach (var instructor in imInstructorDal.GetInstructors())
+{
+    Console.WriteLine(instructor.FirstName + " " + instructor.LastName);
+}
